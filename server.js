5be5a30ddc194a6e95f3900617eae83b44597e2e
rushikeshs-cuelogic <rushikesh.shelar@cuelogic.com>
2017-07-18 12:59:57 +0530

    added schema validations custom error messages, index creation

--- server.js
+++ server.js
@@ -1,39 +1,72 @@
-var app = require('express')();
 require('dotenv').config();
-var mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
+let app = require('express')(),
+    mongoose = require('mongoose'),
+    validate = require('mongoose-validator');
 
 mongoose.connect('mongodb://localhost/Assignment')
     .then(() => console.log('connection succesful'))
     .catch((err) => console.error(err));
 
+// validators for the name and salary
+let nameValidator =
+    validate({
+        validator: 'matches',
+        arguments: /[A-Za-z]/,
+        message: 'name must be letters'
+    }),
+    salaryValidator =
+    validate({
+        validator: 'matches',
+        arguments: /[0-9]/,
+        message: 'salary must be a a whole number containing 0-9'
+    });
 
-var employeeSchema = new mongoose.Schema({
-    name: String,
-    address: String,
-    position: String,
-    salary: Number,
+// create an a schema for employee
+let employeeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        validate: nameValidator,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    position: {
+        type: String,
+        required: true
+    },
+    salary: {
+        type: Number,
+        validate: salaryValidator,
+        required: true
+    },
     updated_at: { type: Date, default: Date.now },
 });
 
-var employeeModel = mongoose.model('Employee', employeeSchema);
+// create model based on schema
+let employeeModel = mongoose.model('Employee', employeeSchema);
 
-
+// create employee 
 function createEmployee() {
-
     let employeeObj = new employeeModel({
-        name: "RDS one",
+        name: "Test",
         address: "10th",
         position: "Sr Software engineer",
-        salary: 2000
+        salary: 98.98
     });
+    // save in db
+    employeeObj.save(function(err) {
 
-    employeeObj.save();
-
-    console.log("Employee Created", employeeObj);
+        if (err.errors.name) {
+            console.log(err.errors.name.message);
+        } else if (err.errors.salary) {
+            console.log(err.errors.salary.message)
+        }
+    });
 };
 
-
+// get employees list
 function getEmployee() {
 
     employeeModel.find({}).exec(function(err, list) {
@@ -48,6 +81,7 @@ function getEmployee() {
     });
 };
 
+// delete employee
 function deleteEmployee(id) {
 
     employeeModel.remove({ _id: id }, function(err) {
@@ -58,11 +92,11 @@ function deleteEmployee(id) {
         };
     });
 };
+
 createEmployee();
 //deleteEmployee("595de9a92dcc9832719d9039");
 getEmployee();
 
-/*app.listen(process.env.PORT, function() {
+app.listen(process.env.PORT, function() {
     console.log('listening on localhost:', process.env.PORT);
 });
-*/
